d711ac495bb0be0630b7290c6f9f9baa6e2d21a4
Aliron <a.taraskin02@gmail.com>
2025-05-05 12:15:43 +0300

    Update Cucumber tests

--- src/test/java/steps/CalcStepDefs.java
+++ src/test/java/steps/CalcStepDefs.java
@@ -13,6 +13,7 @@ public class CalcStepDefs {
     @Given("I have a calculator")
     public void i_have_a_calculator() {
         calc = new Calc();
+        exception = null;
     }
 
     @When("I add {double} and {double}")
@@ -34,15 +35,39 @@ public class CalcStepDefs {
     public void i_divide(double a, double b) {
         try {
             result = calc.divide(a, b);
-            exception = null;
         } catch (Exception e) {
             exception = e;
         }
     }
 
+    @When("I raise {double} to the power of {double}")
+    public void i_raise_to_the_power_of(double a, double b) {
+        result = calc.power(a, b);
+    }
+
+    @When("I take the square root of {double}")
+    public void i_take_the_square_root_of(double a) {
+        try {
+            result = calc.sqrt(a);
+        } catch (Exception e) {
+            exception = e;
+        }
+    }
+
+    @When("I get the absolute value of {double}")
+    public void i_get_the_absolute_value_of(double a) {
+        result = calc.abs(a);
+    }
+
     @Then("the result should be {double}")
     public void the_result_should_be(double expected) {
         assertEquals(expected, result, 0.0001);
+    }
+
+    @Then("an exception should be thrown")
+    public void an_exception_should_be_thrown() {
+        assertNotNull(exception);
+        assertTrue(exception instanceof ArithmeticException);
     }
 
     @Then("an error should be thrown")
--- src/test/resources/features/calc.feature
+++ src/test/resources/features/calc.feature
@@ -24,3 +24,28 @@ Feature: Calculator
     Given I have a calculator
     When I divide 1.0 by 0.0
     Then an error should be thrown
+
+  Scenario: Elevating a number to a power
+    Given I have a calculator
+    When I raise 2 to the power of 3
+    Then the result should be 8
+
+  Scenario: Elevation of a number to degree zero
+    Given I have a calculator
+    When I raise 5 to the power of 0
+    Then the result should be 1
+
+  Scenario: The root of a positive number
+    Given I have a calculator
+    When I take the square root of 9
+    Then the result should be 3
+
+  Scenario: Trying to extract the root of a negative number
+    Given I have a calculator
+    When I take the square root of -4
+    Then an exception should be thrown
+
+  Scenario: Absolute value of a negative number
+    Given I have a calculator
+    When I get the absolute value of -7
+    Then the result should be 7
